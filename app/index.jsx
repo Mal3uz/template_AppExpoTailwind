8dfb09bd401455ccce3fb995bb9833f0e2a0ed6c
Mal3uz <phuongnampham7823@gmail.com>
2024-05-22 10:45:31 +0700

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import {Text, View} from "react-native";
+import React from 'react';
+
+export default function Page() {
+  return (
+   
+      <View className="w-full flex justify-center items-center h-full px-4">
+      
+              <Text className="text-3xl">hello</Text>
+       
+      
+      </View>
+  );
+}
